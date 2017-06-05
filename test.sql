e5adfaeb04174d49a708577ac1e83e0066c69775
Mohan Nalgire <MohanNalgire@users.noreply.github.com>
2017-06-05 15:04:32 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,7 @@
+	
+CREATE TABLE `test` (
+ `test_id` int(4) NOT NULL AUTO_INCREMENT,
+ `test_name` varchar(40) NOT NULL,
+ `test_date` date NOT NULL,
+ PRIMARY KEY (`test_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
